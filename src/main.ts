24c21ec79e4c838b64d0b05c59c5976cad99f42d
strukovd <dmitryi97@yandex.ru>
2024-02-26 20:14:11 +0600

    included @nest/config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+
+	const config = await app.get(ConfigService);
+	const port = config.get("PORT");
+
+	await app.listen(port, ()=>{
+		console.log(`Приложение запущено на порту: ${port}`);
+	});
 }
 bootstrap();
